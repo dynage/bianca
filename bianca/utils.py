f14209c2eb434fded1714264e41ebe6f24985387
fliem <franz.liem@gmail.com>
2020-04-22 14:46:33 +0200

    initial masterfile

---
+++ bianca/utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pathlib import Path
+
+
+def create_masterfile(prep_dir, training_data_dir, bianca_dir, flair_tmpl, t1w_tmpl, manual_mask_tmpl,
+                      mat_tmpl):
+    bianca_dir.mkdir(exist_ok=True, parents=True)
+
+    # get flair files
+    flair_globs = list(prep_dir.glob(flair_tmpl.format(subject="*", session="*")))
+    print(f"{len(flair_globs)} flair files found")
+
+    # get remaining files
+    df = pd.DataFrame({"flair": [str(f) for f in flair_globs]})
+    df["subject"] = df.flair.str.split("sub-").str[1].str.split("/").str[0]
+    df["session"] = df.flair.str.split("ses-").str[1].str.split("/").str[0]
+
+    complete_files = {"t1w": str(prep_dir / t1w_tmpl),
+                      "manual_mask": str(training_data_dir / manual_mask_tmpl),
+                      "mat": str(prep_dir / mat_tmpl)
+                      }
+    for var in complete_files.keys():
+        df[var] = None
+    for i in range(len(df)):
+        for var in complete_files.keys():
+            df[var].iloc[i] = complete_files[var].format(**df.iloc[i])
+
+    df = df[['flair', 't1w', 'manual_mask', 'mat', 'subject', 'session']]
+
+    # only include masks from training subjects
+    training_subject_index = df.manual_mask.apply(lambda s: Path(s).is_file())
+    df.loc[~training_subject_index, "manual_mask"] = None
+    training_subjects = df[training_subject_index]
+
+    # check if files exist
+    # fixme
+    files = df[['flair', 't1w', 'manual_mask', 'mat']].melt()["value"].to_list()
+    for f in files:
+        if f:
+            assert Path(f).is_file(), f"{f} is missing"
+
+    # save
+    df.to_csv(bianca_dir / "masterfile.txt", index=False, header=False, sep=" ")
+    df.to_csv(bianca_dir / "masterfile_wHeader.txt", index=False, sep=" ")
+    training_subjects.to_csv(bianca_dir / "masterfile_training_subjects.txt", index=False, sep=" ")
---
+++ runscripts/training_subjects/s02_create_masterfile.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from bianca.utils import create_masterfile
+
+training_data_dir = Path("/Volumes/lhab_collaboration/WMH/BIANCA/training_data")
+
+####
+# 2D
+acq = "2D"
+space = "flair2D"
+base_dir = Path(f"/Volumes/lhab_collaboration/WMH/BIANCA/training_subjects/{acq}")
+prep_name = "prepare"
+name = "bianca"
+
+prep_dir = base_dir / prep_name
+bianca_dir = base_dir / name
+
+wd_dir = base_dir / "_wd" / name
+crash_dir = base_dir / "_crash" / name
+
+flair_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_acq-" + acq + "_*_FLAIR_biascorr.nii.gz"
+t1w_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_space-" + space + "_desc-t1w_brain.nii.gz"
+manual_mask_tmpl = "sub-{subject}/ses-{session}/sub-{subject}_ses-{session}_acq-" + acq + "_run-1_FLAIR_mask_goldstandard_new.nii.gz"
+mat_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-12dof_from-" + space + "_to-MNI.mat"
+
+create_masterfile(prep_dir, training_data_dir, bianca_dir,
+                  flair_tmpl=flair_tmpl,
+                  t1w_tmpl=t1w_tmpl,
+                  manual_mask_tmpl=manual_mask_tmpl,
+                  mat_tmpl=mat_tmpl)
+
+
+
+####
+# 3D
+acq = "3D"
+space = "flair3D"
+base_dir = Path(f"/Volumes/lhab_collaboration/WMH/BIANCA/training_subjects/{acq}")
+prep_name = "prepare"
+name = "bianca"
+
+prep_dir = base_dir / prep_name
+bianca_dir = base_dir / name
+
+wd_dir = base_dir / "_wd" / name
+crash_dir = base_dir / "_crash" / name
+
+flair_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_acq-" + acq + "_*_FLAIR_biascorr.nii.gz"
+t1w_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_space-" + space + "_desc-t1w_brain.nii.gz"
+manual_mask_tmpl = "sub-{subject}/ses-{session}/sub-{subject}_ses-{session}_acq-" + acq + "_run-1_FLAIR_mask_goldstandard_new.nii.gz"
+mat_tmpl = "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-12dof_from-" + space + "_to-MNI.mat"
+
+create_masterfile(prep_dir, training_data_dir, bianca_dir,
+                  flair_tmpl=flair_tmpl,
+                  t1w_tmpl=t1w_tmpl,
+                  manual_mask_tmpl=manual_mask_tmpl,
+                  mat_tmpl=mat_tmpl)
